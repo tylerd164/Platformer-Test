df8966cf1e101390945c0ea9e2e43cc12128a03e
Jack16907 <jgibso301@caledonian.ac.uk>
2026-03-23 13:40:44 +0000

    Add files via upload

--- Assets/Jack/General Scripts/GameManager.cs
+++ Assets/Jack/General Scripts/GameManager.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
     public GameObject PipesHolder;
-    public GameObject [] Pipes;
+    public GameObject[] Pipes;
     public GameObject GameWinPanel;
 
+    [Header("Stats")]
     public int totalPipes = 0;
-
     public int correctedPipes = 0;
+
     void Start()
     {
+        if(GameWinPanel != null) GameWinPanel.SetActive(false);
+
         totalPipes = PipesHolder.transform.childCount;
         
         Pipes = new GameObject[totalPipes]; 
-
-        for(int i = 0; i < Pipes.Length; i++)
+        for(int i = 0; i < totalPipes; i++)
         {
             Pipes[i] = PipesHolder.transform.GetChild(i).gameObject;
         }
@@ -24,12 +25,26 @@ public class GameManager : MonoBehaviour
 
     public void correctMove()
     {
-        correctedPipes += 1;
+        correctedPipes++;
+        CheckWinCondition();
+    }
 
+    public void wrongMove()
+    {
+        correctedPipes--;
+        
+        if (correctedPipes < 0) correctedPipes = 0;
+    }
+
+    private void CheckWinCondition()
+    {
         if(correctedPipes == totalPipes)
         {
-            Debug.Log("You win!");
-            GameWinPanel.SetActive(true);
+            Debug.Log("Puzzle Solved!");
+            if(GameWinPanel != null)
+            {
+                GameWinPanel.SetActive(true);
+            }
         }
     }
 }
