87def15b1e3e27b016584693c3b0705eabe8325e
Jack16907 <jgibso301@caledonian.ac.uk>
2026-03-30 11:30:44 +0100

    Add files via upload

--- Assets/Jack/General Scripts/GameManager.cs
+++ Assets/Jack/General Scripts/GameManager.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private ControlTerminal controlTerminal;
     public GameObject PipesHolder;
     public GameObject[] Pipes;
+    public GameObject GameWinPanel;
 
     [Header("Stats")]
     public int totalPipes = 0;
     public int correctedPipes = 0;
 
+    [Header("Vibration Settings - Win")]
+    public float winIntensity = 0.8f;
+    public float winDuration = 1.2f;
+
+    [Header("Vibration Settings - Single Correct")]
+    public float clickIntensity = 0.2f;
+    public float clickDuration = 0.1f;
+
     void Start()
     {
+        if(GameWinPanel != null) GameWinPanel.SetActive(false);
+
         totalPipes = PipesHolder.transform.childCount;
         
         Pipes = new GameObject[totalPipes]; 
@@ -24,13 +37,18 @@ public class GameManager : MonoBehaviour
     public void correctMove()
     {
         correctedPipes++;
+        
+        if (correctedPipes < totalPipes)
+        {
+            StartCoroutine(VibrateController(clickIntensity, clickDuration));
+        }
+        
         CheckWinCondition();
     }
 
     public void wrongMove()
     {
         correctedPipes--;
-        
         if (correctedPipes < 0) correctedPipes = 0;
     }
 
@@ -39,7 +57,32 @@ public class GameManager : MonoBehaviour
         if(correctedPipes == totalPipes)
         {
             Debug.Log("Puzzle Solved!");
-            controlTerminal.MiniGameOverUI();
+            
+            if(GameWinPanel != null)
+            {
+                GameWinPanel.SetActive(true);
+            }
+
+            StartCoroutine(VibrateController(winIntensity, winDuration));
+        }
+    }
+
+    IEnumerator VibrateController(float intensity, float duration)
+    {
+        var gamepad = Gamepad.current;
+        if (gamepad != null)
+        {
+            gamepad.SetMotorSpeeds(intensity, intensity);
+            yield return new WaitForSeconds(duration);
+            gamepad.ResetHaptics();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (Gamepad.current != null)
+        {
+            Gamepad.current.ResetHaptics();
         }
     }
 }
